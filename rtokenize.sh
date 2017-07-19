e40f845cc66dd21d4fedcb64bfb5aebaf8ad037f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-07-19 18:44:04 +0000

    lukaszgryglicki-added-logging-to-shell-command

--- rtokenize.sh
+++ rtokenize.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 input=`mktemp`
 output=`mktemp`
+echo "start: $input $output" >> rtokenize.log
+# start: to remove
+# cat
+# rm -f $input $output 2>/dev/null
+# echo "end: $input $output" >> rtokenize.log
+# exit 0
+# end: to remove
 cat > $input
 /home/justa/dev/rtokenize/rtokenize.rb -$1 < $input > $output
 rc=$?
@@ -8,6 +15,7 @@ if [ $rc -ne 0 ]
 then
 	cat $input
 	rm -f $input $output 2>/dev/null
+	echo "err1: $input $output" >> rtokenize.log
 	exit 0
 fi
 /home/justa/dev/rtokenize/rlocalize.rb $1 $output $input
@@ -16,8 +24,10 @@ if [ $rc -ne 0 ]
 then
 	cat $input
 	rm -f $input $output 2>/dev/null
+	echo "err2: $input $output" >> rtokenize.log
 	exit 0
 fi
 cat $output
 rm -f $input $output 2>/dev/null
+echo "end: $input $output" >> rtokenize.log
 exit 0
